d23addb818299a4c628858e2e96e1df3610740b6
Alec Koumjian <akoumjian@gmail.com>
2020-12-01 09:36:27 -0500

    remove f string

--- client/embedding_as_service_client/__init__.py
+++ client/embedding_as_service_client/__init__.py
@@ -8,35 +8,41 @@ class EmbeddingClient(object):
     """
     Represents an example client.
     """
+
     def __init__(self, host, port):
         self.zmq_context = zmq.Context()
         self.socket = self.zmq_context.socket(zmq.DEALER)
-        self.socket.connect(f'tcp://{}:{}'.format(host, port))
-        self.identity = '123'
+        self.socket.connect("tcp://{}:{}".format(host, port))
+        self.identity = "123"
 
-    def encode(self,
-               texts: Union[List[str], List[List[str]]],
-               pooling: Optional[str] = None,
-               is_tokenized: bool = False,
-               batch_size: int = 256,
-               **kwargs
-               ) -> np.array:
+    def encode(
+        self,
+        texts: Union[List[str], List[List[str]]],
+        pooling: Optional[str] = None,
+        is_tokenized: bool = False,
+        batch_size: int = 256,
+        **kwargs
+    ) -> np.array:
         """
         Connects to server. Send compute request, poll for and print result to standard out.
         """
         if not isinstance(texts, list):
-            raise ValueError('Argument `texts` should be either List[str] or List[List[str]]')
+            raise ValueError(
+                "Argument `texts` should be either List[str] or List[List[str]]"
+            )
         if is_tokenized:
             if not all(isinstance(text, list) for text in texts):
-                raise ValueError('Argument `texts` should be List[List[str]] (list of tokens) '
-                                 'when `is_tokenized` = True')
+                raise ValueError(
+                    "Argument `texts` should be List[List[str]] (list of tokens) "
+                    "when `is_tokenized` = True"
+                )
         embeddings = []
         for i in range(0, len(texts), batch_size):
             request_data = {
-                'type': 'encode',
-                'texts': texts[i: i+batch_size],
-                'pooling': pooling,
-                'is_tokenized': is_tokenized
+                "type": "encode",
+                "texts": texts[i : i + batch_size],
+                "pooling": pooling,
+                "is_tokenized": is_tokenized,
             }
             self.send(json.dumps(request_data))
             result = self.receive()
@@ -62,13 +68,10 @@ class EmbeddingClient(object):
         return self.socket.recv()
 
     def tokenize(self, texts: Union[List[str], str]) -> np.array:
-        request_data = {
-            'type': 'tokenize',
-            'texts': texts
-        }
+        request_data = {"type": "tokenize", "texts": texts}
 
         self.send(json.dumps(request_data))
         result = self.receive()
         result = json.loads(result.decode("utf-8"))
-        return result['tokens']
+        return result["tokens"]
 
