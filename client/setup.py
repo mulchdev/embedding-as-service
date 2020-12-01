adc81ff0e587fad586a95aac75e9e3c75c7c8ae1
Alec Koumjian <akoumjian@gmail.com>
2020-12-01 09:25:06 -0500

    Remove encoding arg for py2 compat

--- client/setup.py
+++ client/setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-with open("README.md", "r", encoding='utf-8') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    require_packages = [line[:-1] if line[-1] == '\n' else line for line in f]
+with open("requirements.txt") as f:
+    require_packages = [line[:-1] if line[-1] == "\n" else line for line in f]
 
 setuptools.setup(
     name="embedding_as_service_client",
@@ -19,10 +19,11 @@ setuptools.setup(
     install_requires=require_packages,
     include_package_data=True,
     classifiers=[
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    keywords='bert nlp tensorflow machine learning sentence encoding embedding serving albert glove word2vec'
+    python_requires=">=2.7",
+    keywords="bert nlp tensorflow machine learning sentence encoding embedding serving albert glove word2vec",
 )
